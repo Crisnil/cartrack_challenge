8182922bcbc5e2555ac8ada4a4745374b90b73f9
crisnil <cacuyado@gmail.com>
2022-01-04 14:55:17 +0800

    details

--- src/component/Details.js
+++ src/component/Details.js
@@ -59,7 +59,6 @@ function Details(props) {
 
     const sendEmail = (e) => {
         e.preventDefault();
-            console.log(e,form)
         setLoading(true)
          emailjs.sendForm('service_q5vhlhr', 'template_oh47k8b', form.current, 'user_5i02o9SdsvTxKs0MpcW98')
             .then((result) => {
@@ -91,8 +90,8 @@ function Details(props) {
 
                                 <br/>
                                 <form ref={form}>
-                                    <TextField id="outlined-basic" label="Name" variant="outlined" name={"user_name"} value={state.name} className={classes.name1} disabled/> <br/>
-                                    <TextField id="outlined-basic" label="Email" variant="outlined" name={"user_email"} value={state.email} className={classes.email1} disabled/> <br/>
+                                    <TextField id="outlined-basic"  variant="outlined" name={"user_name"} value={state.name} className={classes.name1} disabled/> <br/>
+                                    <TextField id="outlined-basic" variant="outlined" name={"user_email"} value={state.email} className={classes.email1} disabled/> <br/>
                                     <TextareaAutosize
                                         name = "message"
                                         aria-label="minimum height"
